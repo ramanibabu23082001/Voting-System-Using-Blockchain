bb4b080fd90478967f137afa9f65e9c8538713d7
ramanibabu23082001 <60523818+ramanibabu23082001@users.noreply.github.com>
2021-03-15 13:49:21 +0530

    Delete middleware directory

--- middleware/voterauthentication.js
+++
@@ -1,7 +0,0 @@
-module.exports=(req,res,next)=>{
-    if(!req.session.voter){
-    return res.redirect('/voterlogin/');}
-    if(!req.session.isLoggedIn){
-    return res.redirect('/voterlogin/');}
-    next();
-    }
